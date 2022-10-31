65e1ec2a0dfc023c9ded12e6a981dd4a330a34e1
Trung <trung22112002@gmail.com>
2022-10-31 05:45:02 -0700

    synch added, syscall added,read/print int, readstring

--- nachos-3.4/code/userprog/syscall.h
+++ nachos-3.4/code/userprog/syscall.h
@@ -30,6 +30,13 @@
 #define SC_Fork		9
 #define SC_Yield	10
 
+#define SC_ReadInt  11
+#define SC_PrintInt 12
+#define SC_ReadChar 13
+#define SC_PrintChar    14
+#define SC_ReadString   15
+#define SC_PrintString  16
+
 #ifndef IN_ASM
 
 /* The system call interface.  These are the operations the Nachos
@@ -122,7 +129,10 @@ void Fork(void (*func)());
 /* Yield the CPU to another runnable thread, whether in this address space 
  * or not. 
  */
-void Yield();		
+void Yield();
+
+int ReadInt();
+void PrintInt(int number);
 
 #endif /* IN_ASM */
 
