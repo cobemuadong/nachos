de4b33e43756cf3a04f1f0a43ba658c4b9a293e1
cobemuadong <truong.pht.7080@gmail.com>
2022-11-05 15:42:13 +0700

    commit help file

---
+++ nachos-3.4/code/test/help.c
@@ -0,0 +1,16 @@
+#include "syscall.h"
+
+int main()
+{
+    PrintString("Our team:\n");
+    PrintString("20120224 - Tran Thi My Trinh\n");
+    PrintString("20120228 - Tran Quoc Trung\n");
+    PrintString("20120231 - Phan Huy Truong\n");
+    PrintString("20120247 - Nguyen Phuong Anh\n");
+    PrintString("20120328 - Hoang Duc Nhat Minh\n");
+    PrintString("ASCII: at directory code, run ./userprog/nachos -rs 1023 -x ./test/ascii" 
+    "to print the ASCII table\n");
+    PrintString("Sort: at directory code, run ./userprog/nachos -rs 1023 -x ./test/sort"
+    "to start the sort program\n");
+    return 0;
+}
