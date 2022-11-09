82bf2421f26432645f91a17f6cc8555dee47d019
cobemuadong <truong.pht.7080@gmail.com>
2022-11-09 23:54:34 +0700

    help

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -9,6 +9,6 @@ int main()
     PrintString("20120247 - Nguyen Phuong Anh\n");
     PrintString("20120328 - Hoang Duc Nhat Minh\n");
     PrintString("ASCII: at directory code, run ./userprog/nachos -rs 1023 -x ./test/ascii to print the ASCII table\n");
-    PrintString("Sort: at directory code, run ./userprog/nachos -rs 1023 -x ./test/sort to start the sort program\n");
+    PrintString("Sort: at directory code, run ./userprog/nachos -rs 1023 -x ./test/bubblesort to start the sort program\n");
     return 0;
 }
